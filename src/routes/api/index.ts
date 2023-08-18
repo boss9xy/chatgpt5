057885ae51522ded2e0f94d5ca2a201f46a84ce8
boss9xy <63171333+boss9xy@users.noreply.github.com>
2023-08-19 02:20:06 +0700

    index.ts

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -10,7 +10,7 @@ export const config = {
   /**
    * https://vercel.com/docs/concepts/edge-network/regions#region-list
    * disable hongkong
-   * only for vercer
+   * only for vercel
    */
   regions: [
     "arn1",
